b5adc11786b19835e168adc1dfff1b37f13caa92
s-satsangi <s.satsangi@gmail.com>
2021-01-19 09:51:24 -0500

    users and started on messages as a stub in App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Login from "./components/login";
+import UserForm from "./containers/UserForm";
+import ComposeMessage from "./components/ComposeMessage";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
+      {/* <header className="App-header"> */}
+      {/* <p>
           Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
+        </p> */}
+      {/* <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
-      </header>
+        </a> */}
+      {/* </header> */}
+      <h1> Log In Here </h1>
+      <Login />
+      <h1> New User Here </h1>
+      <UserForm />
+      <h1> Write a Message Here </h1>
+      <ComposeMessage />
     </div>
   );
 }
---
+++ src/components/register.js
@@ -0,0 +1,107 @@
+import React, { useState } from "react";
+import { Container, TextField } from "@material-ui/core";
+
+export default class UserForm extends Component {
+  state = {
+    // firstname: "",
+    // lastname: "",
+    username: "",
+    password: "",
+    passwordConfirm: "",
+    submit: false,
+  };
+
+  createUser = (event) => {
+    event.preventDefault();
+    const data = {
+      user: {
+        // first_name: this.state.firstname,
+        // last_name: this.state.lastname,
+        username: this.state.username,
+        password: this.state.password,
+      },
+    };
+    fetch("http://localhost:3000/users", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(data),
+      credentials: "include",
+    })
+      .then((res) => res.json())
+      .then((json) => {
+        if (json.status) throw json;
+        alert("change to redirect later");
+      })
+      .catch((err) => alert(`${err.message}`));
+    event.target.reset();
+  };
+
+  inputHandler = (event, keyname) => {
+    this.setState({
+      [keyname]: event.target.value,
+    });
+  };
+
+  passwordHandler = (event) => {
+    this.setState({ passwordConfirm: event.target.value });
+    this.state.password === event.target.value
+      ? this.setState({ submit: true })
+      : this.setState({ submit: false });
+  };
+
+  render() {
+    return (
+      <Container>
+        <form onSubmit={this.createUser}>
+          {/* <TextField
+            required
+            label="First Name"
+            className="userform"
+            onChange={(event) => this.inputHandler(event, "firstname")}
+          />
+          <br />
+          <br />
+          <TextField
+            required
+            label="Last Name"
+            className="userform"
+            onChange={(event) => this.inputHandler(event, "lastname")}
+          />
+          <br /> */}
+          <br />
+          <TextField
+            required
+            label="Username"
+            className="userform"
+            onChange={(event) => this.inputHandler(event, "username")}
+          />
+          <br />
+          <br />
+          <TextField
+            required
+            label="Password"
+            className="userform"
+            type="password"
+            onChange={(event) => this.inputHandler(event, "password")}
+          />
+          <br />
+          <br />
+          <TextField
+            required
+            label="Confirm Password"
+            className="userform"
+            type="password"
+            onChange={(event) => this.passwordHandler(event)}
+          />
+          <br />
+          <br />
+          {this.state.submit ? (
+            <TextField type="submit" />
+          ) : (
+            <TextField type="submit" disabled />
+          )}
+        </form>
+      </Container>
+    );
+  }
+}
