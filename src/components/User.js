8e5e1377bc0ff8672d458ca565802ea449ff3130
s-satsangi <s.satsangi@gmail.com>
2021-01-21 22:16:06 -0500

    now we can list all a user's friends

--- src/components/User.js
+++ src/components/User.js
@@ -2,6 +2,9 @@ import React from "react";
 
 export default function User(props) {
   console.log("User");
-
+  // if the user isn't a friend, we should have
+  // a button that says add friend here.
+  // maybe that happens in the AllUsers container?
+  // yeah, maybe we start by building the friendlist?
   return <div>{props.user} howdy</div>;
 }
